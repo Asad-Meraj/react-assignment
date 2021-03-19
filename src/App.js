a7e9760d20083cb74a55e73d34d0f9c8a27caf26
Ahmad <Ahmad@DESKTOP-2A8IG7C>
2021-03-19 18:30:51 +0100

    final project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import Axios from "axios"
+import React, {useState} from "react"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  const [data, setData] = useState ("")
+  const getData = () =>{
+  Axios.get("https://api.met.no/weatherapi/locationforecast/2.0/compact.json?lat=58.4037694&lon=15.5407345").then (
+    (response)=>{
+
+      setData(response.data.properties.meta.updated_at);
+
+
+    });
+};
+  if (data === "") {
+    return (
+      <div className="App">
+        <main className="main"> Main window </main>
+
+        <header className="App-header">
+          <p>This is header</p>
+
+        </header> 
+        <button className={`btn`} onClick={getData}> Click Here to get Data   </button>    
+      </div>
+    ); 
+    
+  } else {
+    return (
+
+      <div className="App">
+        <main className="main"> Main window </main>
+
+        <header className="App-header">
+          {data.replace(/[A-Z]/g, " ")}
+        </header>   
+
+        <button className={`btn`} onClick={getData}> Click Here to get Data   </button>
+      </div>
+    ); 
+
+  }
+  
 }
 
 export default App;
